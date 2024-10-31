7e465e5a62230b5949c1ae930262bfed350e448f
Daniel Blevins <daniel.blevins@asu.edu>
2024-10-30 20:27:18 -0700

    init

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
-from backup_error_webserver.app import app
+try:
+    from .src.backup_error_webserver.app import app
+except ImportError:
+    from backup_error_webserver.app import app
--- src/backup_error_webserver/app.py
+++ src/backup_error_webserver/app.py
@@ -1,16 +1,28 @@
-from flask import Flask
+import os
+import random
+
+from flask import Flask, render_template, render_template_string, request
 
 app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY", random.randbytes(16))
+CONTENT_HTML_FILE = os.getenv("CONTENT_HTML_FILE")
+if CONTENT_HTML_FILE and CONTENT_HTML_FILE.startswith('"'):
+    CONTENT_HTML_FILE = CONTENT_HTML_FILE[1:-1]
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+def get_content():
+    if CONTENT_HTML_FILE:
+        with open(CONTENT_HTML_FILE, "r") as f:
+            return render_template_string(f.read())
+    return render_template("content.html")
 
 
-def run():
-    app.run(debug=True, host="0.0.0.0")
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def index(path):
+    to_req_url = request.url
+    return render_template("index.html", url=to_req_url, content=get_content()), 200
 
 
 if __name__ == "__main__":
-    run()
+    app.run(debug=True, host="0.0.0.0")
